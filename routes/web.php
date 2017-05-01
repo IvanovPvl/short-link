80355733739fb34fd8a8c643f649c26f23b4f30f
Pavel Ivanov <tyrension@gmail.com>
2017-05-01 11:04:17 +0700

    Rename Api to LinkController.

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,7 @@
 $app->get('/', function () use ($app) {
     return $app->version();
 });
+
+$app->group(['prefix' => 'api', 'namespace' => 'Api'], function () use ($app) {
+    $app->post('link', 'LinkController@store');
+});
